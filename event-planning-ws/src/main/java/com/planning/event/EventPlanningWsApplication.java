09ffb4e62f3f27b08f76a9b1cb6502298768a125
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-11-26 15:00:13 -0600

    Initial Project creation

---
+++ event-planning-ws/src/main/java/com/planning/event/EventPlanningWsApplication.java
@@ -0,0 +1,12 @@
+package com.planning.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanningWsApplication.class, args);
+	}
+}
